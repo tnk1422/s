ab684aeb1c451469f3931f977b5079005ab47680
tnk1422 <tarak.konar@gmail.com>
2021-12-18 22:54:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import  Flask,render_template,request
 app=Flask(__name__)
 @app.route("/")
 def index():
-    image=request.args.get("images/cover.jpg")
+    #image=request.args.get("images/cover.jpg")
     return render_template("basic.html",image=image)
 @app.route("/solids")
 def solids():  
